d58afc92e68fe83c67f3a2f1e6bf65f6bbac0496
MARK DAVE BALADAD <20260309@s.ubaguio.edu>
2026-03-23 11:06:57 +0800

    Update App.tsx

--- student-portal/src/App.tsx
+++ student-portal/src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import About from './pages/About';
+import BookRequest from './pages/BookRequest';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import EventFeedback from './pages/EventFeedback';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <div className="py-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/book-request" element={<BookRequest />} />
+          <Route path="/event-feedback" element={<EventFeedback />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
